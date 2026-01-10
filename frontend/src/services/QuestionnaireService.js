324f4e6bff8aa662a8a6e42df046d5615ec74915
Piotr Średnicki <piotrsrednicki@bluerider.software>
2026-01-10 19:46:24 +0100

    Add QuestionnaireService for API interactions and update Vite config for path aliases and proxy

---
+++ frontend/src/services/QuestionnaireService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_BASE = 'http://localhost:8000/api/questionnaire';
+
+export default {
+  list: () => axios.get(`${API_BASE}/`),
+  create: (data) => axios.post(`${API_BASE}/`, data),
+  update: (id, data) => axios.put(`${API_BASE}/${id}/`, data),
+  delete: (id) => axios.delete(`${API_BASE}/${id}/`),
+  getRandomColour: () => axios.get(`${API_BASE}/random-colour/`),
+};
