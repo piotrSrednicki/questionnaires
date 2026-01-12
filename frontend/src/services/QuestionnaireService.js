02e4f895e96b27cf3334d696fb987b67a285fc3b
Piotr Średnicki <piotrsrednicki@bluerider.software>
2026-01-12 13:32:51 +0100

    Add get method to QuestionnaireService for fetching individual questionnaires

--- frontend/src/services/QuestionnaireService.js
+++ frontend/src/services/QuestionnaireService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const API_BASE = 'http://localhost:8000/api/questionnaire';
 
 export default {
+  get: (id) => axios.get(`${API_BASE}/${id}/`),
   list: () => axios.get(`${API_BASE}/`),
   create: (data) => axios.post(`${API_BASE}/`, data),
   update: (id, data) => axios.put(`${API_BASE}/${id}/`, data),
